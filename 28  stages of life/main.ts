e527387f5259ad90ed7d488cf699db97fa5fb5f6
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:34:46 +0500

    28 stage of life

---
+++ 28  stages of life/main.ts
@@ -0,0 +1,26 @@
+let age = 35 ;
+
+if (age < 2) {
+    console.log("you are baby");
+    
+}
+else if(age >= 2 && age < 4){
+    console.log("you are toddler");
+    
+}
+else if(age >= 4 && age < 13){
+    console.log("you are kid");
+    
+}
+else if(age >= 13 && age < 20){
+    console.log("you are teenager");
+    
+}
+else if(age >= 20 && age < 65){
+    console.log("you are adult");
+    
+}
+else if(age >= 65){
+    console.log("you are elder");
+    
+}
