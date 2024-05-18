a955184edd99c13d8036de48b2cacc081dd9384c
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:39:06 +0500

    34 pizzas

---
+++ 34 pizzas/main.js
@@ -0,0 +1,6 @@
+var pizza = ["chicken tikka", "malai boti", "fajita"];
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onePizza = pizza_1[_i];
+    console.log("I Like ".concat(onePizza, " pizza"));
+}
+console.log("pizza is love");
---
+++ 34 pizzas/main.ts
@@ -0,0 +1,7 @@
+let pizza = ["chicken tikka", "malai boti", "fajita"]
+
+for(let onePizza of pizza){
+    console.log(`I Like ${onePizza} pizza`);
+}
+
+console.log("pizza is love");
