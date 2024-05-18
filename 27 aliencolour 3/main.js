e0270301f9859dde71023734f5a7f8d2449c20fa
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:34:11 +0500

    27 alien colour 3

---
+++ 27 aliencolour 3/main.js
@@ -0,0 +1,30 @@
+var alienColour = "green";
+if (alienColour === "green") {
+    console.log("you shoot green alien you got 5 points");
+}
+else if (alienColour === "yellow") {
+    console.log("you shoot yellow alien you got 10 points");
+}
+else if (alienColour === "red") {
+    console.log("you shoot red alien you got 15 points");
+}
+var alienColour2 = "yellow";
+if (alienColour2 === "green") {
+    console.log("you shoot green alien you got 5 points");
+}
+else if (alienColour2 === "yellow") {
+    console.log("you shoot yellow alien you got 10 points");
+}
+else if (alienColour2 === "red") {
+    console.log("you shoot red alien you got 15 points");
+}
+var alienColour3 = "red";
+if (alienColour3 === "green") {
+    console.log("you shoot green alien you got 5 points");
+}
+else if (alienColour3 === "yellow") {
+    console.log("you shoot yellow alien you got 10 points");
+}
+else if (alienColour3 === "red") {
+    console.log("you shoot red alien you got 15 points");
+}
---
+++ 27 aliencolour 3/main.ts
@@ -0,0 +1,45 @@
+let alienColour = "green";
+
+if (alienColour === "green") {
+    console.log("you shoot green alien you got 5 points");
+    
+}
+else if(alienColour === "yellow"){
+    console.log("you shoot yellow alien you got 10 points");
+    
+}
+else if(alienColour === "red"){
+    console.log("you shoot red alien you got 15 points");
+    
+}
+
+let alienColour2 = "yellow";
+
+if (alienColour2 === "green") {
+    console.log("you shoot green alien you got 5 points");
+    
+}
+else if(alienColour2 === "yellow"){
+    console.log("you shoot yellow alien you got 10 points");
+    
+}
+else if(alienColour2 === "red"){
+    console.log("you shoot red alien you got 15 points");
+    
+}
+
+
+let alienColour3 = "red";
+
+if (alienColour3 === "green") {
+    console.log("you shoot green alien you got 5 points");
+    
+}
+else if(alienColour3 === "yellow"){
+    console.log("you shoot yellow alien you got 10 points");
+    
+}
+else if(alienColour3 === "red"){
+    console.log("you shoot red alien you got 15 points");
+    
+}
