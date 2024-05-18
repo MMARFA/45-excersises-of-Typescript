b3f98687657aac4eb704073b1eb805214936fbe8
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:25:03 +0500

    15 modifying array

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 var guestList = ["Arfat", "kamran", "Asif"];
+var dontCome = guestList[1];
+console.log(dontCome, "nai aha sakta");
+guestList.splice(0, 1, "hussain");
 guestList.forEach(function (guestList) { return console.log("salam ".concat(guestList, " , would you like to dinner with me?")); });
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 let guestList = ["Arfat", "kamran", "Asif"];
 
+let dontCome = guestList[1]
+
+console.log(dontCome, "nai aha sakta");
+
+guestList.splice(0, 1, "hussain");
+
 guestList.forEach(guestList => console.log(`salam ${guestList} , would you like to dinner with me?`));
