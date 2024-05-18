d2b23cdc6e964ceedb45d73182263171f0acbb06
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:28:59 +0500

    20 store in array

---
+++ 20store in array/main.js
@@ -0,0 +1,3 @@
+var dreamBike = ["H2r", "Hayabusa", "Bmw", "gxsr"];
+console.log("list of dream bikes");
+//dreamBike.forEach(bike =>console.log(bike));
---
+++ 20store in array/main.ts
@@ -0,0 +1,5 @@
+let dreamBike = ["H2r", "Hayabusa", "Bmw", "gxsr"]
+
+console.log("list of dream bikes");
+
+dreamBike.forEach(bike =>console.log(bike));
