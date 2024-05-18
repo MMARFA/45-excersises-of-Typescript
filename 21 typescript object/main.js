200c41f0f0c57afe5f7baae4d96da23f3f4f1e83
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:29:48 +0500

    type script object

---
+++ 21 typescript object/main.js
@@ -0,0 +1,6 @@
+var itCourse = {
+    courseName: "Type script and Java script",
+    location: "governer house",
+    onsiteStudent: 50000
+};
+console.log(itCourse);
---
+++ 21 typescript object/main.ts
@@ -0,0 +1,12 @@
+interface itCourse {
+    courseName : string;
+    location : string;
+    onsiteStudent : number;
+}
+
+let itCourse = {
+    courseName : "Type script and Java script",
+    location : "governer house",
+    onsiteStudent : 50000
+}
+console.log(itCourse);
