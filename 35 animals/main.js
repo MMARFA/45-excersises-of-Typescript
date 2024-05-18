26739ca218450c5f68734f68b2d686f2fe7e2ce3
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:40:06 +0500

    35 animals

---
+++ 35 animals/main.js
@@ -0,0 +1,6 @@
+var wildAnimals = ["Tigars", "Bear", "leopards"];
+for (var _i = 0, wildAnimals_1 = wildAnimals; _i < wildAnimals_1.length; _i++) {
+    var oneWild = wildAnimals_1[_i];
+    console.log("A ".concat(oneWild, " would make a great wild."));
+}
+console.log("Any of these animals would make great wild");
---
+++ 35 animals/main.ts
@@ -0,0 +1,8 @@
+let wildAnimals = ["Tigars", "Bear", "leopards"]
+
+for(let oneWild of wildAnimals){
+    console.log(`A ${oneWild} would make a great wild.`);
+    
+}
+
+console.log("Any of these animals would make great wild");
