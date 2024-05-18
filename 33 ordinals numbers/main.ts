e778dab3b620eefd5555d63f5d8254ae843233c8
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:38:07 +0500

    33 ordinal numbers

---
+++ 33 ordinals numbers/main.ts
@@ -0,0 +1,19 @@
+let numbers = [1 , 2 , 3, 4, 5, 6, 7, 8, 9]
+
+for(let oneNumber of numbers){
+    let ordinalEnding: string; 
+    if (oneNumber === 1) {
+        ordinalEnding ="st"
+    }
+    else if (oneNumber === 2){
+ordinalEnding = "nd"
+    }
+    else if(oneNumber === 3){
+ordinalEnding ="rd"
+    }
+    else{
+        ordinalEnding = "th"
+    }
+    console.log(`${oneNumber}${ordinalEnding}`);
+    
+}
