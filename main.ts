1463e94452191985136dec3646574232f166c5d5
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:25:42 +0500

    16 modifying array

--- main.ts
+++ main.ts
@@ -6,4 +6,12 @@ console.log(dontCome, "nai aha sakta");
 
 guestList.splice(0, 1, "hussain");
 
+console.log("Good news ! we found a big table for dinner");
+
+guestList.unshift("Atif")
+guestList.push("sufiyan")
+let middleIndex: number = Math.floor (guestList.length / 2)
+guestList.splice(middleIndex, 0, "Ahmed")
+console.log("updated list for guest");
+
 guestList.forEach(guestList => console.log(`salam ${guestList} , would you like to dinner with me?`));
