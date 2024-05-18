ca62f4ac5398177c1eed3bad0c51e0f242194c03
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:32:19 +0500

    25 alien colour

---
+++ 25 alien colour/main.ts
@@ -0,0 +1,9 @@
+let alienColour = "purple";
+
+if (alienColour === "purple") {
+    console.log("players just earn 5 point");
+}
+
+if (alienColour === "blue") {
+    console.log("Alien colour is blue");
+}
