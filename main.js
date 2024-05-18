158f8ea7e8d7a85da4ef62c60b55e216581ea6ba
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:26:30 +0500

    17 shriking guest list

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-var guestList = ["Arfat", "kamran", "Asif"];
+var guestList = ["Arfat", "kamran", "atif"];
 var dontCome = guestList[1];
 console.log(dontCome, "nai aha sakta");
 guestList.splice(0, 1, "hussain");
 console.log("Good news ! we found a big table for dinner");
-guestList.unshift("Atif");
+guestList.unshift("asif");
 guestList.push("sufiyan");
 var middleIndex = Math.floor(guestList.length / 2);
 guestList.splice(middleIndex, 0, "Ahmed");
 console.log("updated list for guest");
 guestList.forEach(function (guestList) { return console.log("salam ".concat(guestList, " , would you like to dinner with me?")); });
+console.log("unfortunataly , the new dinner table wont arrive to timeso it can only invite two guest to dinner with me");
+while (guestList.length > 2) {
+    var removeGuest = guestList.pop();
+    console.log("sorry , ".concat(removeGuest, " i cant invite you to dinner"));
+}
+console.log("invitations to the last to guest");
+guestList.forEach(function (lasttwo) { return console.log("luckly ".concat(lasttwo, " you are still invited for the dinner")); });
+guestList.pop();
+guestList.pop();
+console.log("Empty list:", guestList);
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let guestList = ["Arfat", "kamran", "Asif"];
+let guestList = ["Arfat", "kamran", "atif"];
 
 let dontCome = guestList[1]
 
@@ -8,10 +8,28 @@ guestList.splice(0, 1, "hussain");
 
 console.log("Good news ! we found a big table for dinner");
 
-guestList.unshift("Atif")
+guestList.unshift("asif")
 guestList.push("sufiyan")
 let middleIndex: number = Math.floor (guestList.length / 2)
 guestList.splice(middleIndex, 0, "Ahmed")
 console.log("updated list for guest");
 
 guestList.forEach(guestList => console.log(`salam ${guestList} , would you like to dinner with me?`));
+
+console.log("unfortunataly , the new dinner table wont arrive to timeso it can only invite two guest to dinner with me");
+
+while (guestList.length > 2) {
+    let removeGuest = guestList.pop();
+    console.log(`sorry , ${removeGuest} i cant invite you to dinner`);
+    
+}
+
+console.log("invitations to the last to guest");
+
+guestList.forEach (lasttwo => console.log(`luckly ${lasttwo} you are still invited for the dinner`)
+)
+
+guestList.pop();
+guestList.pop();
+
+console.log("Empty list:",guestList);
