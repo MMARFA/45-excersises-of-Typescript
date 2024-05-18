d77198b1b35c77ba8724ca84e019b44161aa7a33
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:33:25 +0500

    26 alien colour 2

---
+++ 26 alien colour 2/main.js
@@ -0,0 +1,13 @@
+var alienColour = "purple";
+if (alienColour === "purple") {
+    console.log("you just earn 5 points");
+}
+else {
+    console.log(" you earn 10 points");
+}
+if (alienColour === "blue") {
+    console.log("you just earn 5 points");
+}
+else {
+    console.log(" you earn 10 points");
+}
