6b08c1565d2ea25b1aa4074c8d1808f1b44c97b4
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:35:57 +0500

    29 favourite fruit

---
+++ 29 favourite fruit/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let favourite_fruit = ["mango", "banana", "orange"];
+if (favourite_fruit.includes("mango")) {
+    console.log("I really like mangoes");
+}
+if (favourite_fruit.includes("strawberry")) {
+    console.log("I really like strawberrys");
+}
+if (favourite_fruit.includes("banana")) {
+    console.log("I really like banana");
+}
+if (favourite_fruit.includes("apple")) {
+    console.log("I really like apple");
+}
+if (favourite_fruit.includes("orange")) {
+    console.log("I really like orange");
+}
