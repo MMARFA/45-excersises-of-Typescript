8842fe696360d299bdd47370e586ab7a841e746a
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:44:42 +0500

    41 magications

---
+++ 41 magication/main.js
@@ -0,0 +1,5 @@
+function show_magicians(magicians) {
+    magicians.forEach(function (name) { return console.log(name); });
+}
+var magicians_names = ["harry porter", "arfat", "asif"];
+show_magicians(magicians_names);
---
+++ 41 magication/main.ts
@@ -0,0 +1,6 @@
+function show_magicians(magicians: string[]){
+    magicians.forEach(name => console.log(name));   
+}
+
+let magicians_names = ["harry porter", "arfat", "asif"]
+show_magicians(magicians_names)
