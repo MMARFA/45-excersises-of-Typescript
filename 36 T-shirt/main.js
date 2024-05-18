dfdb4ed4271834d9bbfea381e36cd795e8a25227
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:41:23 +0500

    36 t shirts

---
+++ 36 T-shirt/main.js
@@ -0,0 +1,4 @@
+function make_shirt(size, printMessage) {
+    console.log("you selected ".concat(size, " shirt with ").concat(printMessage, " prints on shirt"));
+}
+make_shirt("Medium", "Pakistan zindabad");
---
+++ 36 T-shirt/main.ts
@@ -0,0 +1,4 @@
+function make_shirt (size: string, printMessage: string){
+    console.log(`you selected ${size} shirt with ${printMessage} prints on shirt`);
+}  
+make_shirt("Medium", "Pakistan zindabad")
