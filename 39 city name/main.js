8930ce7bb5bb08f7c6080df1d6da0aca8bb9303f
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:43:19 +0500

    39 city names

---
+++ 39 city name/main.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country, " ");
+}
+console.log(city_country("karachi", "pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("dehli", "india"));
---
+++ 39 city name/main.ts
@@ -0,0 +1,9 @@
+function city_country(city: string, country: string) : string{
+    return `${city}, ${country} `;
+}
+
+console.log(city_country("karachi" , "pakistan"));
+
+console.log(city_country("Tokyo","Japan"));
+
+console.log(city_country("dehli", "india"));
