97bcd10f77c913295f25e934b83d5edf28b6caf5
MMARFA <160514577+MMARFA@users.noreply.github.com>
2024-05-19 00:28:23 +0500

    19 dinner guest

---
+++ 19 dinner guest/main.js
@@ -0,0 +1,4 @@
+var guestList = ["Arfat", "kamran", "Asif"];
+//guestList.forEach(guestList => console.log(`salam ${guestList} , would you like to dinner with me?`));
+var lengthGuests = guestList.length;
+console.log("We are inviting total ".concat(lengthGuests, " guest"));
---
+++ 19 dinner guest/main.ts
@@ -0,0 +1,8 @@
+let guestList = ["Arfat", "kamran", "Asif"];
+
+//guestList.forEach(guestList => console.log(`salam ${guestList} , would you like to dinner with me?`));
+
+let lengthGuests : number = guestList.length;
+
+
+console.log(`We are inviting total ${lengthGuests} guest`);
